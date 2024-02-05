5fc7061b11737c75868513d22bd6d51bbba9f7ac
Abhiram <abhiram.nandiraju@gmail.com>
2024-02-05 01:11:19 -0600

    flask fully working ready to replace main branch

--- app.py
+++ app.py
@@ -4,7 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Assuming you have an index.html in your templates folder
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/chapters')
+def chapters():
+    return render_template('chapters.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/guest-lectures')
+def guest_lectures():
+    return render_template('guest-lectures.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
